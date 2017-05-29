b12f9e7aada63f6e10b054cc52d86b9b5fd3bc26
albetaCOM <alba.prades@gmail.com>
2017-05-29 10:47:43 +0200

    Added videostrem funcionality from pstuart
    https://github.com/pstuart/Generic-Video-Camera/tree/master/devicetypes/pstuart/generic-video-camera.src

--- st_foscam_universal.groovy
+++ st_foscam_universal.groovy
@@ -1,18 +1,42 @@
 /**
  *  Foscam Universal Device
  *
- *  Copyright 2014 skp19
+ *  Copyright 2017 albetaCOM
  *
+ *  modified 2017-05-25 :  albetaCOM   - Added videostream functioanlity from pstuart:
+ *					https://github.com/pstuart/Generic-Video-Camera/tree/master/devicetypes/pstuart/generic-video-camera.src
+ *  modified 2017-05-20 :  albetaCOM   - Change to work with Foscam 8910W.
+ *  modified 2015-06-04 :  thrash99er  - changed bool comparsions from string to bool  i.e.  "true" to true
+ *
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ * 
  */
 metadata {
-	definition (name: "Foscam Universal Device", namespace: "skp19", author: "skp19") {
+	definition (name: "Foscam Universal Device with video stream and PTZ", namespace: "albetaCOM", author: "Alba Prades Doria") {
 		capability "Polling"
 		capability "Image Capture"
+		capability "Configuration"
+		capability "Video Camera"
+		capability "Video Capture"
+		capability "Refresh"
+		capability "Switch"
+
         
         attribute "alarmStatus", "string"
         attribute "ledStatus",   "string"
         attribute "hubactionMode", "string"
     
+   		// custom commands
+		command "start"
+
 		command "alarmOn"
 		command "alarmOff"
 		command "toggleAlarm"
@@ -44,22 +68,53 @@ metadata {
         input("hdcamera", "bool", title:"HD Foscam Camera? (9xxx Series)", description: "Type of Foscam Camera", required: true, displayDuringSetup: true)
         input("mirror", "bool", title:"Mirror? (Not required for HD cameras)", description: "Camera Mirrored?")
         input("flip", "bool", title:"Flip? (Not required for HD cameras)", description: "Camera Flipped?")
-		input("preset1", "text", title: "Preset 1 (For HD cameras only)", description: "Name of your first preset position")
-		input("preset2", "text", title: "Preset 2 (For HD cameras only)", description: "Name of your second preset position")
-		input("preset3", "text", title: "Preset 3 (For HD cameras only)", description: "Name of your third preset position")
-		input("cruisemap1", "text", title: "Cruise Map 1 (For HD cameras only. Non-HD cameras will default to Horizontal.)", description: "Name of your first cruise map", defaultValue: "Horizontal")
-		input("cruisemap2", "text", title: "Cruise Map 2 (For HD cameras only. Non-HD cameras will default to Vertical.)", description: "Name of your second cruise map", defaultValue: "Vertical")
+        input("CameraStreamPathCustom","string", title: "Camera Stream Path", description: "Please enter your camera's streaming path", defaultValue: settings?.CameraStreamPathList, required:false, displayDuringSetup: true)
 	}
 
 	tiles {
-        carouselTile("cameraDetails", "device.image", width: 3, height: 2) { }
+    	multiAttributeTile(name: "videoPlayer", type: "videoPlayer", width: 6, height: 4) {
+			tileAttribute("device.switch", key: "CAMERA_STATUS") {
+				attributeState("on", label: "Active", icon: "st.camera.dlink-indoor", action: "switch.off", backgroundColor: "#79b821", defaultState: true)
+				attributeState("off", label: "Inactive", icon: "st.camera.dlink-indoor", action: "switch.on", backgroundColor: "#ffffff")
+				attributeState("restarting", label: "Connecting", icon: "st.camera.dlink-indoor", backgroundColor: "#53a7c0")
+				attributeState("unavailable", label: "Unavailable", icon: "st.camera.dlink-indoor", action: "refresh.refresh", backgroundColor: "#F22000")
+			}
+
+			tileAttribute("device.errorMessage", key: "CAMERA_ERROR_MESSAGE") {
+				attributeState("errorMessage", label: "", value: "", defaultState: true)
+			}
+
+			tileAttribute("device.camera", key: "PRIMARY_CONTROL") {
+				attributeState("on", label: "Active", icon: "st.camera.dlink-indoor", backgroundColor: "#79b821", defaultState: true)
+				attributeState("off", label: "Inactive", icon: "st.camera.dlink-indoor", backgroundColor: "#ffffff")
+				attributeState("restarting", label: "Connecting", icon: "st.camera.dlink-indoor", backgroundColor: "#53a7c0")
+				attributeState("unavailable", label: "Unavailable", icon: "st.camera.dlink-indoor", backgroundColor: "#F22000")
+			}
+
+			tileAttribute("device.startLive", key: "START_LIVE") {
+				attributeState("live", action: "start", defaultState: true)
+			}
+
+			tileAttribute("device.stream", key: "STREAM_URL") {
+				attributeState("activeURL", defaultState: true)
+			}
+            /*
+			tileAttribute("device.profile", key: "STREAM_QUALITY") {
+				attributeState("1", label: "720p", action: "setProfileHD", defaultState: true)
+				attributeState("2", label: "h360p", action: "setProfileSDH", defaultState: true)
+				attributeState("3", label: "l360p", action: "setProfileSDL", defaultState: true)
+			}	*/		
+		}
+
+    
+        carouselTile("cameraDetails", "device.image", width: 4, height: 3) { }
 
         standardTile("camera", "device.alarmStatus", width: 1, height: 1, canChangeIcon: true, inactiveLabel: true, canChangeBackground: true) {
           state "off", label: "off", action: "toggleAlarm", icon: "st.camera.dropcam-centered", backgroundColor: "#FFFFFF"
           state "on", label: "on", action: "toggleAlarm", icon: "st.camera.dropcam-centered",  backgroundColor: "#53A7C0"
         }
 
-		standardTile("take", "device.image", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
+		standardTile("take", "device.image", width: 2, height: 2, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
 			state "take", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF", nextState:"taking"
 			state "taking", label:'Taking', action: "", icon: "st.camera.take-photo", backgroundColor: "#53a7c0"
 			state "image", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF", nextState:"taking"
@@ -77,7 +132,7 @@ metadata {
           state "manual", label: "manual", action: "toggleLED", icon: "st.Lighting.light13", backgroundColor: "#FFFF00"
         }
         
-        standardTile("ledAuto", "device.ledStatus", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
+        standardTile("ledAuto", "device.ledStatus", width: 2, height: 2, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
           state "auto", label: "auto", action: "ledAuto", icon: "st.Lighting.light11", backgroundColor: "#53A7C0"
           state "off", label: "auto", action: "ledAuto", icon: "st.Lighting.light13", backgroundColor: "#FFFFFF"
           state "on", label: "auto", action: "ledAuto", icon: "st.Lighting.light13", backgroundColor: "#FFFFFF"
@@ -98,31 +153,11 @@ metadata {
           state "manual", label: "off", action: "ledOff", icon: "st.Lighting.light13", backgroundColor: "#00FF00"
         }
         
-		standardTile("preset1", "device.image", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false, decoration: "flat") {
-			state "preset1", label: "preset 1", action: "preset1", icon: ""
-		}
-
-		standardTile("preset2", "device.image", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false, decoration: "flat") {
-			state "preset2", label: "preset 2", action: "preset2", icon: ""
-		}
-
-		standardTile("preset3", "device.image", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false, decoration: "flat") {
-			state "preset3", label: "preset 3", action: "preset3", icon: ""
-		}
+        standardTile("refresh", "device.alarmStatus", inactiveLabel: false, decoration: "flat") {
+          state "refresh", action:"polling.poll", icon:"st.secondary.refresh"
+        }
         
-		standardTile("cruisemap1", "device.image", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false, decoration: "flat") {
-			state "cruisemap1", label: "Cruise Map 1", action: "cruisemap1", icon: ""
-		}
-
-		standardTile("cruisemap2", "device.image", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false, decoration: "flat") {
-			state "cruisemap2", label: "Cruise Map 2", action: "cruisemap2", icon: ""
-		}
- 
- 		standardTile("stopcruise", "device.image", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false, decoration: "flat") {
-			state "stopcruise", label: "Stop Cruise", action: "stopCruise", icon: ""
-		}
-
-		standardTile("left", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false, decoration: "flat") {
+        standardTile("left", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false, decoration: "flat") {
 			state "left", label: "left", action: "left", icon: ""
 		}
 
@@ -138,21 +173,14 @@ metadata {
 			state "down", label: "down", action: "down", icon: "st.thermostat.thermostat-down"
 		}
 
-		standardTile("stop", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false, decoration: "flat") {
-			state "stop", label: "", action: "stopCruise", icon: "st.sonos.stop-btn"
-		}
-
-        standardTile("refresh", "device.alarmStatus", inactiveLabel: false, decoration: "flat") {
-          state "refresh", action:"polling.poll", icon:"st.secondary.refresh"
-        }
-        
         standardTile("blank", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false, decoration: "flat") {
           state "blank", label: "", action: "", icon: "", backgroundColor: "#FFFFFF"
         }
 
         main "camera"
-			//details(["cameraDetails", "take", "blank", "alarmStatus", "ledAuto", "ledOn", "ledOff", "refresh"]) //**Uncomment this line and comment out the next line to hide the PTZ controls
-			details(["cameraDetails", "take", "blank", "alarmStatus", "ledAuto", "ledOn", "ledOff", "preset1", "preset2", "preset3", "cruisemap1", "cruisemap2", "stopcruise", "blank", "up", "blank", "left", "stop", "right", "blank", "down", "blank", "refresh"])
+			//details(["videoPlayer", "cameraDetails", "take", "ledStatus", "refresh"]) //**Uncomment this line and comment out the next line to hide the PTZ controls        
+			details(["videoPlayer", "cameraDetails", "take", "up",  "down", "refresh", "ledStatus", "blank", "blank",  "left", "right"])
+
 	}
 }
 
@@ -160,7 +188,7 @@ metadata {
 def take() {
 	log.debug("Taking Photo")
 	sendEvent(name: "hubactionMode", value: "s3");
-    if(hdcamera == "true") {
+    if(hdcamera == true) {
 		hubGet("cmd=snapPicture2")
     }
     else {
@@ -168,7 +196,31 @@ def take() {
     }
 }
 //END TAKE PICTURE
+def configure() {
+	log.debug "Executing 'configure'"
+    sendEvent(name:"switch", value: "on")
+}
 
+def start() {
+	log.trace "start()"
+	def dataLiveVideo = [
+		OutHomeURL  : CameraStreamPathCustom,
+		InHomeURL   : CameraStreamPathCustom,
+		ThumbnailURL: "http://cdn.device-icons.smartthings.com/camera/dlink-indoor@2x.png",
+		cookie      : [key: "key", value: "value"]
+	]
+
+	def event = [
+		name           : "stream",
+		value          : groovy.json.JsonOutput.toJson(dataLiveVideo).toString(),
+		data		   : groovy.json.JsonOutput.toJson(dataLiveVideo),
+		descriptionText: "Starting the livestream",
+		eventType      : "VIDEO",
+		displayed      : false,
+		isStateChange  : true
+	]
+	sendEvent(event)
+}
 //ALARM ACTIONS
 def toggleAlarm() {
 	log.debug "Toggling Alarm"
@@ -183,8 +235,8 @@ def toggleAlarm() {
 def alarmOn() {
 	log.debug "Enabling Alarm"
     sendEvent(name: "alarmStatus", value: "on");
-    if(hdcamera == "true") {
-		hubGet("cmd=setMotionDetectConfig&isEnable=1")
+    if(hdcamera == true) {
+       hubGet("cmd=setMotionDetectConfig&isEnable=1")
     }
     else {
     	hubGet("/set_alarm.cgi?motion_armed=1&")
@@ -194,7 +246,7 @@ def alarmOn() {
 def alarmOff() {
 	log.debug "Disabling Alarm"
     sendEvent(name: "alarmStatus", value: "off");
-    if(hdcamera == "true") {
+    if(hdcamera == true) {
 		hubGet("cmd=setMotionDetectConfig&isEnable=0")
     }
     else {
@@ -224,7 +276,7 @@ def toggleLED() {
 def ledOn() {
     log.debug("LED changed to: on")
     sendEvent(name: "ledStatus", value: "on");
-    if(hdcamera == "true") {
+    if(hdcamera == true) {
 	    delayBetween([hubGet("cmd=setInfraLedConfig&mode=1"), hubGet("cmd=openInfraLed")])
     }
     else {
@@ -246,7 +298,7 @@ def ledOff() {
 def ledAuto() {
     log.debug("LED changed to: auto")
     sendEvent(name: "ledStatus", value: "auto");
-	if(hdcamera == "true") {
+	if(hdcamera == true) {
 		hubGet("cmd=setInfraLedConfig&mode=0")
     }
     else {
@@ -254,71 +306,6 @@ def ledAuto() {
     }
 }
 //END LED ACTIONS
-
-//PRESET ACTIONS
-def preset1() {
-	log.debug("Preset 1 Selected - ${preset1}")
-	if(hdcamera == "true") {
-		hubGet("cmd=ptzGotoPresetPoint&name=${preset1}")
-    }
-    else {
-    	hubGet("/decoder_control.cgi?command=31&")
-    }
-}
-
-def preset2() {
-	log.debug("Preset 2 Selected - ${preset2}")
-	if(hdcamera == "true") {
-		hubGet("cmd=ptzGotoPresetPoint&name=${preset2}")
-    }
-    else {
-    	hubGet("/decoder_control.cgi?command=33&")
-    }
-}
-
-def preset3() {
-	log.debug("Preset 3 Selected - ${preset3}")
-	if(hdcamera == "true") {
-		hubGet("cmd=ptzGotoPresetPoint&name=${preset3}")
-    }
-    else {
-    	hubGet("/decoder_control.cgi?command=35&")
-    }
-}
-//END PRESET ACTIONS
-
-//CRUISE ACTIONS
-def cruisemap1() {
-	log.debug("Cruise Map 1 Selected - ${cruisemap1}")
-	if(hdcamera == "true") {
-		hubGet("cmd=ptzStartCruise&mapName=${cruisemap1}")
-    }
-    else {
-    	hubGet("/decoder_control.cgi?command=28&")
-    }
-}
-
-def cruisemap2() {
-	log.debug("Cruise Map 2 Selected - ${cruisemap2}")
-	if(hdcamera == "true") {
-		hubGet("cmd=ptzStartCruise&mapName=${cruisemap2}")
-    }
-    else {
-    	hubGet("/decoder_control.cgi?command=26&")
-    }
-}
-
-def stopCruise() {
-	log.debug("Stop Cruise")
-	if(hdcamera == "true") {
-		hubGet("cmd=ptzStopRun")
-    }
-    else {
-    	delayBetween([hubGet("/decoder_control.cgi?command=29&"), hubGet("/decoder_control.cgi?command=27&")])
-    }
-}
-//END CRUISE ACTIONS
-
 //PTZ CONTROLS
 def left() {
 	if(hdcamera == "true") {
@@ -349,11 +336,11 @@ def right() {
 }
 
 def up() {
-	if(hdcamera == "true") {
+	if(hdcamera == true) {
         delayBetween([hubGet("cmd=ptzMoveUp"), hubGet("cmd=ptzStopRun")])
     }
     else {
-    	if(flip == "true") {
+    	if(flip == true) {
 	    	hubGet("/decoder_control.cgi?command=2&onestep=1&")
         }
         else {
@@ -363,11 +350,11 @@ def up() {
 }
 
 def down() {
-	if(hdcamera == "true") {
+	if(hdcamera == true) {
         delayBetween([hubGet("cmd=ptzMoveDown"), hubGet("cmd=ptzStopRun")])
     }
     else {
-    	if(flip == "true") {
+    	if(flip == true) {
     		hubGet("/decoder_control.cgi?command=0&onestep=1&")
         }
         else {
@@ -377,11 +364,12 @@ def down() {
 }
 //END PTZ CONTROLS
 
+
 def poll() {
 
 	sendEvent(name: "hubactionMode", value: "local");
     //Poll Motion Alarm Status and IR LED Mode
-    if(hdcamera == "true") {
+    if(hdcamera == true) {
 		delayBetween([hubGet("cmd=getMotionDetectConfig"), hubGet("cmd=getInfraLedConfig")])
     }
     else {
@@ -390,7 +378,7 @@ def poll() {
 }
 
 private getLogin() {
-	if(hdcamera == "true") {
+	if(hdcamera == true) {
     	return "usr=${username}&pwd=${password}&"
     }
     else {
@@ -407,7 +395,7 @@ private hubGet(def apiCommand) {
 
 	log.debug("Executing hubaction on " + getHostAddress())
     def uri = ""
-    if(hdcamera == "true") {
+    if(hdcamera == true) {
     	uri = "/cgi-bin/CGIProxy.fcgi?" + getLogin() + apiCommand
 	}
     else {
@@ -435,14 +423,17 @@ def parse(String description) {
     def descMap = parseDescriptionAsMap(description)
         
     //Image
-	if (descMap["bucket"] && descMap["key"]) {
-		putImageInS3(descMap)
-	}
-
+    if (descMap["tempImageKey"]) {
+        try {
+            storeTemporaryImage(descMap["tempImageKey"], getPictureName())
+        } catch (Exception e) {
+            log.error e
+        }
+    }
 	//Status Polling
     else if (descMap["headers"] && descMap["body"]) {
         def body = new String(descMap["body"].decodeBase64())
-        if(hdcamera == "true") {
+        if(hdcamera == true) {
             def langs = new XmlSlurper().parseText(body)
 
             def motionAlarm = "$langs.isEnable"
